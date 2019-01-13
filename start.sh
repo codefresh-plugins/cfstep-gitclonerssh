9664f84658f2f6b7301c8a53d0baea0dcb648a87
francisco-codefresh <francisco@codefresh.io>
2019-01-13 01:33:13 -0600

    Fix

--- start.sh
+++ start.sh
@@ -42,7 +42,7 @@ cd $CLONE_PATH/$REPO_NAME
 if [ "$BRANCH" != "master" ]; then
   echo "Checking out $BRANCH"
   git fetch --all
-  git checkout $BRANCH && git branch && git status
+  git checkout -b $BRANCH  --track origin/$BRANCH && git branch && git status
 fi
 
 echo "ls -alh $CLONE_PATH/$REPO_NAME"
